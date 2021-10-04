8030ea2f52099bf3415127b99b366e899be6b920
Alexander Maletin <man.alex212@gmail.com>
2021-10-04 19:37:08 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int total = currentBalance + transferAmount;
+        System.out.println(total);
+    }
+}
